f66dc396b8678d1f53aa997d30f10485205d08a1
SZEKIN <958845478@qq.com>
2019-05-29 16:30:42 +0800

    书架类

---
+++ app/src/main/java/com/liurui/bookshelf/Shelf.java
@@ -0,0 +1,18 @@
+package com.liurui.bookshelf;
+
+/**
+ * Created by szekinfung on 2019/5/29.
+ */
+
+public class Shelf {
+    private String shelf;
+
+    public String getShelf() {
+        return shelf;
+    }
+
+    public void setShelf(String shelf) {
+        this.shelf = shelf;
+    }
+
+}
