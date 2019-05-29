74af5cc9bf07a1df98af6638c80c2274e8cd906d
SZEKIN <958845478@qq.com>
2019-05-29 19:39:32 +0800

    自增的图书id已经完成

--- app/src/main/java/com/liurui/bookshelf/AddBook.java
+++ app/src/main/java/com/liurui/bookshelf/AddBook.java
@@ -1,19 +1,39 @@
 package com.liurui.bookshelf;
 
+import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class AddBook {
-    //BookCollection bookCollection = new BookCollection();
-    public Book add_book(String name)
-    {
+    Context context;
+    private int sid;
+    Collection collection_sid = new Collection("SID");
+    ArrayList<Integer> one_int = new ArrayList<>();
+
+    public AddBook(Context context){
+        this.context = context;
+        ArrayList<Integer> tmp_sid = collection_sid.read(this.context);
+        if(tmp_sid.size()==0)
+            sid = 0;
+        else
+            sid = tmp_sid.get(0);
+    }
+
+    public Book add_book(String name) {
         Book book = new Book();
+        book.setId(sid++);
+        ArrayList<Integer> list_sid = new ArrayList<>();
+        list_sid.add(sid);
+        collection_sid.save(context,list_sid);
+        Log.d("fuck",String.valueOf(sid));
+
         book.setName(name);
         book.setAuthor("testAuthor");
         book.setPublishing_house("testpublisher");
         book.setPublishing_time("testtime");
-
         return book;
     }
 
